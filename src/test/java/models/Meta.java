0da1eaf4c15272f6c8a0cd6d43e3778194853d98
L-Surkov <ilya.surkov11@gmail.com>
2026-01-26 08:47:24 +0300

    Add ignore annotation to Meta

--- src/test/java/models/Meta.java
+++ src/test/java/models/Meta.java
@@ -1,10 +1,12 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class Meta {
     @JsonProperty("powered_by")
